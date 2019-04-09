b255b0c557e49d8bab79c82a123cddbf49d2fa39
Sergey <poletvnebesa@yandex.ru>
2019-04-09 13:44:30 +0300

    Start making app

---
+++ SocialApp-Remastered-/Coordinator/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  SocialApp-Remastered-
+//
+//  Created by Sergey Mikhailov on 09/04/2019.
+//  Copyright © 2019 Sergey Mikhailov. All rights reserved.
+//
+
+import Foundation
